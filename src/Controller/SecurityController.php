7ad46e62fd117a719dc1b6309f37e0ccdaefce7f
dev33 <antony.hopkins008@gmail.com>
2018-03-22 20:23:58 +0200

    implemented login form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+    class SecurityController extends Controller
+    {
+        /**
+         * @Route("/login", name="login")
+         *
+         * @param AuthenticationUtils $authenticationUtils
+         *
+         * @return Response
+         */
+        public function login(AuthenticationUtils $authenticationUtils)
+        {
+            $error = $authenticationUtils->getLastAuthenticationError();
+
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+            return $this->render(
+                'security/login.html.twig',
+                [
+                    'last_username' => $lastUsername,
+                    'error'         => $error,
+                ]
+            );
+        }
+    }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,91 +1,191 @@
 <?php
 
-namespace App\Entity;
+    namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ORM\HasLifecycleCallbacks()
- */
-class User
-{
-    public function __construct() {
-        $this->posts = new ArrayCollection();
-    } 
-    
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\ORM\Mapping as ORM;
+    use Serializable;
+    use Symfony\Component\Security\Core\User\UserInterface;
 
     /**
-     * @ORM\Column(type="string", length=100, unique=true)
+     * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+     * @ORM\HasLifecycleCallbacks()
      */
-    private $name;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Post", mappedBy="post") 
-     * 
-     * @var array 
-     */
-    private $posts;
-    
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
-
-    /**
-     * @return array
-     */
-    public function getPosts(): array {
-        return $this->posts;
-    }
-
-    /**
-     * @param array $posts
-     *
-     * @return User 
-     */
-    public function setPosts(array $posts): User {
-        $this->posts = $posts;
-        
-        return $this;
-    }
-   
-    public function getId()
+    class User implements UserInterface, Serializable
     {
-        return $this->id;
+        /**
+         * @ORM\Id()
+         * @ORM\GeneratedValue()
+         * @ORM\Column(type="integer")
+         */
+        private $id;
+
+        /**
+         * @ORM\Column(type="string", length=100, unique=true)
+         */
+        private $username;
+
+        /**
+         * @ORM\Column(type="string", length=64)
+         */
+        private $password;
+
+        /**
+         * @ORM\Column(type="string", length=254, unique=true)
+         */
+        private $email;
+
+        /**
+         * @ORM\Column(name="is_active", type="boolean")
+         */
+        private $isActive;
+
+        /**
+         * @ORM\OneToMany(targetEntity="App\Entity\Post", mappedBy="post")
+         *
+         * @var array
+         */
+        private $posts;
+
+        /**
+         * @ORM\Column(type="datetime")
+         */
+        private $createdAt;
+
+        /**
+         * User constructor.
+         */
+        public function __construct()
+        {
+            $this->posts    = new ArrayCollection();
+            $this->isActive = true;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getEmail()
+        {
+            return $this->email;
+        }
+
+        /**
+         * @param mixed $email
+         */
+        public function setEmail($email): void
+        {
+            $this->email = $email;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getIsActive()
+        {
+            return $this->isActive;
+        }
+
+        /**
+         * @param mixed $isActive
+         */
+        public function setIsActive($isActive): void
+        {
+            $this->isActive = $isActive;
+        }
+
+        /**
+         * @return array
+         */
+        public function getPosts(): array
+        {
+            return $this->posts;
+        }
+
+        /**
+         * @param array $posts
+         *
+         * @return User
+         */
+        public function setPosts(array $posts): User
+        {
+            $this->posts = $posts;
+
+            return $this;
+        }
+
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        public function setUsername(string $name): self
+        {
+            $this->username = $name;
+
+            return $this;
+        }
+
+        public function getCreatedAt(): ?\DateTimeInterface
+        {
+            return $this->createdAt;
+        }
+
+        /**
+         * @ORM\PrePersist
+         */
+        public function setCreatedAt(): self
+        {
+            $this->createdAt = new \DateTime();
+
+            return $this;
+        }
+
+        public function getRoles(): array
+        {
+            return ['ROLE_USER'];
+        }
+
+        public function getPassword(): string
+        {
+            return $this->password;
+        }
+
+        /**
+         * @param mixed $password
+         */
+        public function setPassword($password): void
+        {
+            $this->password = $password;
+        }
+
+        public function getSalt()
+        {
+            return null;
+        }
+
+        public function getUsername(): string
+        {
+            return $this->username;
+        }
+
+        public function eraseCredentials()
+        {
+        }
+
+        public function serialize(): string 
+        {
+            return serialize(
+                [
+                    $this->id,
+                    $this->username,
+                    $this->password,
+
+                ]
+            );
+        }
+
+        public function unserialize($serialized)
+        {
+            list($this->id, $this->username, $this->password) = unserialize($serialized);
+        }
     }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @ORM\PrePersist
-     */
-    public function setCreatedAt(): self
-    {
-        $this->createdAt = new \DateTime();
-
-        return $this;
-    }
-}
